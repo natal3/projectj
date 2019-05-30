2107d7b3c5b622f6acb127fdbef1b7aff052cd91
LEONIDAS GIANNOULIS <masterleo@gmail.com>
2019-05-30 11:28:14 +0200

    antrage buttons

--- src/pages/Itemlp/Antrage.vue
+++ src/pages/Itemlp/Antrage.vue
@@ -1,30 +1,65 @@
 <template>
   <q-page padding>
     <div class="row q-gutter-md">
-      <div class="col" >
+      <div class="col">
         <h6>Antrag Kläger</h6>
-        <q-input
-          filled
-          type="textarea"
-          class="q-mb-md"
-        />
-        <q-input
-          filled
-          class="q-mb-md"
-          type="textarea"
-        />
-        <q-input
-          filled
-          class="q-mb-md"
-          type="textarea"
-        />
+        <div class="row">
+          <div class="col-11">
+            <q-input
+              filled
+              type="textarea"
+              class="q-mb-md"
+            />
+          </div>
+          <div class="col-1">
+            <q-btn flat icon="check"></q-btn>
+            <q-btn flat icon="close"></q-btn>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-11">
+            <q-input
+              filled
+              type="textarea"
+              class="q-mb-md"
+            />
+          </div>
+          <div class="col-1">
+            <q-btn flat icon="check"></q-btn>
+            <q-btn flat icon="close"></q-btn>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-11">
+            <q-input
+              filled
+              type="textarea"
+              class="q-mb-md"
+            />
+          </div>
+          <div class="col-1">
+            <q-btn flat icon="check"></q-btn>
+            <q-btn flat icon="close"></q-btn>
+          </div>
+        </div>
       </div>
       <div class="col">
         <h6>Antrag Beklagter</h6>
-        <q-input
-          filled
-          type="textarea"
-        />
+        <div class="row">
+          <div class="col-11">
+            <q-input
+              filled
+              type="textarea"
+              class="q-mb-md"
+            />
+          </div>
+          <div class="col-1">
+            <q-btn flat icon="check"></q-btn>
+            <q-btn flat icon="close"></q-btn>
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
